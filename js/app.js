7592983388d5ae4459a93b0d16d865e5046abb06
Franco Davolio <francodavolio12@gmail.com>
2022-08-10 09:34:41 -0300

    por fin el tp-05-01 terminado jajaja

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 let numAleatorio = 0;
+let numero = 0;
 function generarNumeroAleatorio() {
-   numAleatorio = Math.round(Math.random() * (0 - 100) + 100);
-   console.log(numAleatorio);
+  numAleatorio = Math.round(Math.random() * (0 - 10) + 10);
+  console.log(numAleatorio);
 }
 
-function numeroMagico(){
-   let numUsuario = parseInt(prompt("Ingrese el numero que usted piensa que va a salir."));
-   if (numAleatorio === numUsuario) {
-      alert("Felicitaciones usted adivino el Numero.");
-   }else(alert("No advino el numero."));
+function numeroMagico() {
+  numero = document.getElementById("num").value;
+  console.log(numero);
+  if (numAleatorio == numero) {
+    alert("Felicitaciones usted adivino el Numero.");
+  } else if (numAleatorio < numero) {
+    alert("El numero es menor al que usted ingreso.");
+  } else if (numAleatorio > numero) {
+      alert("El numero es mayor al que usted ingreso.")
+  }
 }
-
-
-
-
 
